a96ac712d24244e9d0d3e1368fba156f42f61730
Lucas Couto <loc.unb@gmai.com>
2021-08-15 15:04:06 -0300

    Segregacao em modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { RouterModule } from '@angular/router';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: 'courses', component: CourseListComponent
       },
       {
         path: '**', component: Error404Component
